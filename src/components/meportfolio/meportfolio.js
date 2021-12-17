a58f4a497f6b45d4f4f015ce2005e7b01f5a1a83
Aleksandrgit config --global user.email  aleksrowing232@gmail.com <aleksrowing232@gmail.com>
2021-12-17 11:04:11 +0300

    hj

--- src/components/meportfolio/meportfolio.js
+++ src/components/meportfolio/meportfolio.js
@@ -3,6 +3,7 @@ import react from './react.jpg';
 import myapp from './myapp.png';
 import climbers from './climbers.png';
 import calc from './calc.png';
+import './meportfolio.css';
 
 export default class Meportfolio extends Component{
     render(){
@@ -13,16 +14,21 @@ export default class Meportfolio extends Component{
         <div className="diviader diviader_resume"><span></span></div>
         <div className="container">
             <div className="work__me" id="work__me">
-                <div className="workelem"><a href="https://aleksandr232.github.io/rowingtatarstan.io/"><img
+                <div className="workelem"><a href="https://github.com/Aleksandr232/rowingtatarstan.io"><button className='workm'><img src="https://img.icons8.com/ios/50/000000/github--v2.gif" style={{width:'30px' }}/></button></a>
+                <a href="https://aleksandr232.github.io/rowingtatarstan.io/"><img
                             src="https://im.kommersant.ru/Issues.photo/LifeStyle_Online/2019/06/27/KMO_120232_20616_1_t218_153158.jpg"
                             width="248px" height="248px" alt=""/></a></div>
-                <div className="workelem"><a href="https://aleksandr232.github.io/number.io/"><img src={react}
+                <div className="workelem"><a href="https://github.com/Aleksandr232/number.io"><button className='workm'><img src="https://img.icons8.com/ios/50/000000/github--v2.gif" style={{width:'30px' }}/></button></a>
+                <a href="https://aleksandr232.github.io/number.io/"><img src={react}
                             width="248px" height="248px" alt=""/></a></div>
-                 <div className="workelem"><a href="https://aleksandr232.github.io/add-employees.io/"><img src={myapp}
+                 <div className="workelem"><a href="https://github.com/Aleksandr232/add-employees.io"><button className='workm'><img src="https://img.icons8.com/ios/50/000000/github--v2.gif" style={{width:'30px' }}/></button></a>
+                 <a href="https://aleksandr232.github.io/add-employees.io/"><img src={myapp}
                                 width="248px" height="248px" alt=""/></a></div>
-                <div className="workelem"><a href="http://snowkzn.ru/"><img src={climbers}
+                <div className="workelem"><a href="https://github.com/Aleksandr232/climbers"><button className='workm'><img src="https://img.icons8.com/ios/50/000000/github--v2.gif" style={{width:'30px' }}/></button></a>
+                <a href="http://snowkzn.ru/"><img src={climbers}
                                 width="248px" height="248px" alt=""/></a></div>
-                <div className="workelem"><a href="https://calc-io-gamma.vercel.app/"><img src={calc}
+                <div className="workelem"><a href="https://github.com/Aleksandr232/calc.io"><button className='workm'><img src="https://img.icons8.com/ios/50/000000/github--v2.gif" style={{width:'30px' }}/></button></a>
+                <a href="https://calc-io-gamma.vercel.app/"><img src={calc}
                                 width="248px" height="248px" alt=""/></a></div>
             </div>
         </div>
