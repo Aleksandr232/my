641562cebc4ae7ff2c1c2e4797f2964837d8e48d
Aleksandrgit config --global user.email  aleksrowing232@gmail.com <aleksrowing232@gmail.com>
2022-01-30 20:51:01 +0300

    newproject

--- src/components/meportfolio/meportfolio.js
+++ src/components/meportfolio/meportfolio.js
@@ -4,6 +4,7 @@ import myapp from './myapp.png';
 import climbers from './climbers.png';
 import calc from './calc.png';
 import coffe from './coffe.png';
+import grev from './grev.png';
 import './meportfolio.css';
 
 export default class Meportfolio extends Component{
@@ -33,6 +34,9 @@ export default class Meportfolio extends Component{
                                 width="248px" height="248px" alt=""/></a></div>
                 <div className="workelem"><a href="https://github.com/Aleksandr232/coffe"><button className='workm'><img src="https://img.icons8.com/ios/50/000000/github--v2.gif" style={{width:'30px' }}/></button></a>
                 <a href="https://coffe-iota.vercel.app/"><img src={coffe}
+                                width="248px" height="248px" alt=""/></a></div>
+                <div className="workelem"><a href="https://github.com/Aleksandr232/grovemade"><button className='workm'><img src="https://img.icons8.com/ios/50/000000/github--v2.gif" style={{width:'30px' }}/></button></a>
+                <a href="https://grovemade.vercel.app/"><img src={grev}
                                 width="248px" height="248px" alt=""/></a></div>
             </div>
         </div>
